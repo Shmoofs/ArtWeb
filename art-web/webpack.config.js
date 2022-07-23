cb1eb1c0e23f0632662c3e25d07fe0234a881043
Shmoofs <timothyalsaudi@gmail.com>
2022-07-22 18:30:02 -0700

    Setting up bootstrap (it's mad at me :( )

--- art-web/webpack.config.js
+++ art-web/webpack.config.js
@@ -1,32 +1,42 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAAvPJfr2vYg3mYbO3Lbo-RBFqezE2GtG4",
-  authDomain: "artweb-2d885.firebaseapp.com",
-  projectId: "artweb-2d885",
-  storageBucket: "artweb-2d885.appspot.com",
-  messagingSenderId: "728843305018",
-  appId: "1:728843305018:web:f69d8b60914c6d896df503",
-  measurementId: "G-32CHLQJCSP"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/js/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
   },
-};
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 8080,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
